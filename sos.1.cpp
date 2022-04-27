6dc78d6d6e8e64d4bd406b46941453c87629286a
sonsoughoon <gkgkgktjdgns@gmail.com>
2022-04-27 19:16:05 +0900

    PULL안됬었던거 집에서 다시 정리

--- sos.1.cpp
+++ sos.1.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 
-using namespace std;
+using namespace std; //이거 쓰면 일일이 std안써도 됨
 
 int main()
 {
-	cout << "Hallo World" << endl;
-	return 0;
+	int Money = 0; //int(선언), ( = 0; (초기화))*필수임
+	Money = 100;
+
+	float A = 1.0f; //실수 표현할때 쓰는것 f붙이는거 *필수
+
+	char ch = 'a';  //Bayt "a"도 아스키코드이다. 출력될때 아스키코드(숫자)로 출력됨
+
+	cout << (char)(ch) << endl; //"(char)"은 (ch)를 아스키코드(문자)로 출력하게 해줌
+
+
+	return 0; //종료
 }
